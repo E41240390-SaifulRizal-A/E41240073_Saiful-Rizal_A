8ef6c9d0c28af87f5f9ecc9c86336d3ea1a5c25f
E41240390-SaifulRizal-A <mzhafran270@gmail.com>
2025-02-06 10:24:56 +0700

    Create Hasil_pengerjaan_tugas_Minggu_ke_1.java

---
+++ Hasil_pengerjaan_tugas_Minggu_ke_1.java
@@ -0,0 +1,16 @@
+
+package hasil_pengerjaan_tugas_minggu_ke_1;
+
+public class Hasil_pengerjaan_tugas_Minggu_ke_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 10;
+        int b = 9;
+        
+        int hasil = 0 ;
+        System.out.printf( "hasil = "+ i + b);            
+    }
+}
